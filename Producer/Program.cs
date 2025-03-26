7615c12f8e066390871f5a4013c7091793d3d7cc
Xiao-Alatus <113570899+Xiao-Alatus@users.noreply.github.com>
2025-03-26 19:21:58 +0800

    feat: TCP socket connection to pass vids

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+using System.Net;
+
+namespace STDISCM_PS_3___Networked_Producer_and_Consumer
+{
+    class Producer
+    {
+        static void Main()
+        {
+            int port = 9000;
+            string videoPath = "./vidFolder1/test1"; // Make sure this file exists
+
+            try
+            {
+                TcpListener listener = new TcpListener(IPAddress.Any, port);
+                listener.Start();
+                Console.WriteLine("Producer is waiting for connection...");
+
+                using TcpClient client = listener.AcceptTcpClient();
+                using NetworkStream stream = client.GetStream();
+                using FileStream fileStream = File.OpenRead(videoPath);
+
+                fileStream.CopyTo(stream);
+                Console.WriteLine("Video sent to consumer.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
