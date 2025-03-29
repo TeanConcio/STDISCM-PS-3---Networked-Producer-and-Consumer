7c83dd256768dccf74b1a2fff43d3c9767b61d3d
Tean Concio <tean_concio@dlsu.edu.ph>
2025-03-30 05:13:24 +0800

    add: added config handling

--- Consumer/MainWindow.xaml.cs
+++ Consumer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Windows;
@@ -19,11 +20,20 @@ namespace STDISCM_PS_3___Networked_Producer_and_Consumer;
 /// </summary>
 public partial class MainWindow : Window
 {
+    // Default values for configurations
+    private const uint DEFAULT_NUMBER_OF_CONSUMER_THREADS = 4;
+    private const uint DEFAULT_MAX_QUEUE_SIZE = 4;
+    private static readonly IPAddress DEFAULT_PRODUCER_IP_ADDRESS = IPAddress.Parse("127.0.0.1");
+    private const uint DEFAULT_PRODUCER_PORT_NUMBER = 9000;
+
+    // Configurations
+    private static uint numConsumerThreads = DEFAULT_NUMBER_OF_CONSUMER_THREADS;
+    private static uint maxQueueSize = DEFAULT_MAX_QUEUE_SIZE;
+    private static IPAddress producerIPAddress = DEFAULT_PRODUCER_IP_ADDRESS;
+    private static uint producerPortNumber = DEFAULT_PRODUCER_PORT_NUMBER;
 
     private TcpClient client;
     private NetworkStream stream;
-    private const string producerIP = "127.0.0.1";  // Localhost for testing; change to actual IP
-    private const int port = 9000;
 
     private const string videoFolder = "./downloaded_videos";
     private string selectedVideoPath;
@@ -35,6 +45,101 @@ public partial class MainWindow : Window
         LoadVideoList();
     }
 
+    public static void GetConfig()
+    {
+        Console.WriteLine("Getting Configurations from config.txt");
+        Console.WriteLine();
+
+        bool hasErrorWarning = false;
+
+        string[] lines = System.IO.File.ReadAllLines("config.txt");
+        foreach (string line in lines)
+        {
+            // Skip empty lines or comments (#)
+            if (line.Trim() == "" || line.Trim().StartsWith("#"))
+            {
+                continue;
+            }
+
+            // Split line by "="
+            string[] parts = line.Split("=");
+
+            switch (parts[0].Trim().ToUpper())
+            {
+                case "NUMBER_OF_CONSUMER_THREADS (C) = ":
+                    if (!uint.TryParse(parts[1].Trim(), out uint tempNumConsumerThreads) || tempNumConsumerThreads > int.MaxValue || tempNumConsumerThreads < 1)
+                    {
+                        Console.WriteLine($"Error: Invalid Number of Instances. Setting Number of Instances to {DEFAULT_NUMBER_OF_CONSUMER_THREADS}.");
+                        hasErrorWarning = true;
+                    }
+                    else
+                    {
+                        // Check if very large number of instances
+                        if (tempNumConsumerThreads >= 50)
+                        {
+                            Console.WriteLine($"Warning: Very large number of threads. Please expect a very long initialization time.");
+                            hasErrorWarning = true;
+                        }
+
+                        numConsumerThreads = tempNumConsumerThreads;
+                    }
+                    break;
+
+                case "MAX_QUEUE_SIZE (Q) = ":
+                    if (!uint.TryParse(parts[1].Trim(), out uint tempMaxQueueSize) || tempMaxQueueSize > int.MaxValue || tempMaxQueueSize < 1)
+                    {
+                        Console.WriteLine($"Error: Invalid Max Queue Size. Setting Max Queue Size to {DEFAULT_MAX_QUEUE_SIZE}.");
+                        hasErrorWarning = true;
+                    }
+                    else
+                    {
+                        maxQueueSize = tempMaxQueueSize;
+                    }
+                    break;
+
+                case "PRODUCER_IP_ADDRESS = ":
+                    if (!IPAddress.TryParse(parts[1].Trim(), out IPAddress tempProducerIPAddress))
+                    {
+                        Console.WriteLine($"Error: Invalid Producer IP Address. Setting Producer IP Address to {DEFAULT_PRODUCER_IP_ADDRESS}.");
+                        hasErrorWarning = true;
+                    }
+                    else
+                    {
+                        producerIPAddress = tempProducerIPAddress;
+                    }
+                    break;
+
+                case "PRODUCER_PORT_NUMBER = ":
+                    if (!ushort.TryParse(parts[1].Trim(), out ushort tempConsumerPortNumber) || tempConsumerPortNumber > ushort.MaxValue || tempConsumerPortNumber < 1)
+                    {
+                        Console.WriteLine($"Error: Invalid Producer Port Number. Setting Producer Port Number to {DEFAULT_PRODUCER_PORT_NUMBER}.");
+                        hasErrorWarning = true;
+                    }
+                    else
+                    {
+                        producerPortNumber = tempConsumerPortNumber;
+                    }
+                    break;
+            }
+        }
+
+        // If there is an error, print a line
+        if (hasErrorWarning)
+        {
+            Console.WriteLine();
+        }
+
+        // Print Configurations
+        Console.WriteLine("Number of Consumer Threads: " + numConsumerThreads);
+        Console.WriteLine("Max Queue Size: " + maxQueueSize);
+        Console.WriteLine("Producer IP Address: " + producerIPAddress);
+        Console.WriteLine("Producer Port Number: " + producerPortNumber);
+
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+
+
     private async void connectBtn_Click(object sender, RoutedEventArgs e)
     {
         // Give message box to user
@@ -44,7 +149,7 @@ public partial class MainWindow : Window
         try
         {
             client = new TcpClient();
-            await client.ConnectAsync(producerIP, port);
+            await client.ConnectAsync(producerIPAddress, (int)producerPortNumber);
             stream = client.GetStream();
             //logTextBox.AppendText("✅ Connected to producer.\n");
         }
--- Producer/Program.cs
+++ Producer/Program.cs
@@ -5,10 +5,86 @@ namespace STDISCM_PS_3___Networked_Producer_and_Consumer
 {
     class Producer
     {
+        // Default values for configurations
+        private const uint DEFAULT_NUMBER_OF_PRODUCER_THREADS = 4;
+        private const uint DEFAULT_PRODUCER_PORT_NUMBER = 9000;
+
+        // Configurations
+        private static uint numProducerThreads = DEFAULT_NUMBER_OF_PRODUCER_THREADS;
+        private static uint portNumber = DEFAULT_PRODUCER_PORT_NUMBER;
+
+        public static void GetConfig()
+        {
+            Console.WriteLine("Getting Configurations from config.txt");
+            Console.WriteLine();
+
+            bool hasErrorWarning = false;
+
+            string[] lines = System.IO.File.ReadAllLines("config.txt");
+            foreach (string line in lines)
+            {
+                // Skip empty lines or comments (#)
+                if (line.Trim() == "" || line.Trim().StartsWith("#"))
+                {
+                    continue;
+                }
+
+                // Split line by "="
+                string[] parts = line.Split("=");
+
+                switch (parts[0].Trim().ToUpper())
+                {
+                    case "NUMBER_OF_PRODUCER_THREADS (C) = ":
+                        if (!uint.TryParse(parts[1].Trim(), out uint tempNumProducerThreads) || tempNumProducerThreads > int.MaxValue || tempNumProducerThreads < 1)
+                        {
+                            Console.WriteLine($"Error: Invalid Number of Instances. Setting Number of Instances to {DEFAULT_NUMBER_OF_PRODUCER_THREADS}.");
+                            hasErrorWarning = true;
+                        }
+                        else
+                        {
+                            // Check if very large number of instances
+                            if (tempNumProducerThreads >= 50)
+                            {
+                                Console.WriteLine($"Warning: Very large number of threads. Please expect a very long initialization time.");
+                                hasErrorWarning = true;
+                            }
+
+                            numProducerThreads = tempNumProducerThreads;
+                        }
+                        break;
+
+                    case "PRODUCER_PORT_NUMBER = ":
+                        if (!ushort.TryParse(parts[1].Trim(), out ushort tempPortNumber) || tempPortNumber > ushort.MaxValue || tempPortNumber < 1)
+                        {
+                            Console.WriteLine($"Error: Invalid Producer Port Number. Setting Procuder Port Number to {DEFAULT_PRODUCER_PORT_NUMBER}.");
+                            hasErrorWarning = true;
+                        }
+                        else
+                        {
+                            portNumber = tempPortNumber;
+                        }
+                        break;
+                }
+            }
+
+            // If there is an error, print a line
+            if (hasErrorWarning)
+            {
+                Console.WriteLine();
+            }
+
+            // Print Configurations
+            Console.WriteLine("Number of Producer Threads: " + numProducerThreads);
+            Console.WriteLine("Producer Port Number: " + portNumber);
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
         static void Main()
         {
             int port = 9000;
-            string videoPath = "./video_folder_1/test1.mp4"; // Make sure this file exists
+            string videoPath = "./video_folders/video_folder_1/test1.mp4"; // Make sure this file exists
 
             try
             {
